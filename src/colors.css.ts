5b90b8a7d359366c8042cad80cbe352d0087c991
tonguwin <toddnguyen@Todds-MBP.attlocal.net>
2024-04-14 21:56:18 -0700

    test test

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,16 +1,11 @@
 export const colors = {
-<<<<<<< HEAD
   background: "#ebebeb",
   text: "#131313",
   primary: "#131313",
   muted: "#f14e41",
   active: "#6495ED",
-=======
-  background: "#f03e2f",
-  text: "#000000",
-  primary: "#000000",
-  muted: "#f14e41",
-  active: "#e6e6e6",
->>>>>>> origin/main
   black: "#000",
 }
+
+
+//test
